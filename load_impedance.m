ef9bb958f472e0b0590db5626ea9378f02bb1b88
orjan1992 <orjan1992@hotmail.com>
2017-02-09 22:51:49 +0100

    NVA done, changes to Z in 1 and 2

--- load_impedance.m
+++ load_impedance.m
@@ -11,54 +11,54 @@ if BT == 1
     if CB(4) == 0
         %CB4 Open
         %Calculating load impedance
-        Z_l(:, 1) = [par.motor1.Z_m par.motor1.Z_m par.motor1.Z_m]';
+        Z_l(1) = par.motor1.Z_m;
     else
         %CB4 Closed => load impedance = infinite
-        Z_l(:, 1) = [Inf Inf Inf]';
+        Z_l(1) = Inf;
     end
     
     %STARBOARD BUS
-    Z_l(:, 1) = [0 0 0]';
+    Z_l(1) = 0;
     
     if CB(5) == 0
         %CB5 Open
-        Z_l(:, 2) = [par.motor2.Z_m par.motor2.Z_m par.motor2.Z_m]';
+        Z_l(2) = par.motor2.Z_m;
     end
     
     if CB(6) == 0
         %CB6 Open
-         Z_l(:, 2) = Z_l(:, 2) + [par.utility.Z_La_prime par.utility.Z_La_prime par.utility.Z_La_prime]';
+         Z_l(2) = Z_l(2) + par.utility.Z_La_prime;
     end
    
     %Check if there are no loads connected and if so impendance is infinite
-    if sum(Z_l(:, 2)==0) ~= 0
-        Z_l(:, 2) = [Inf Inf Inf]';
+    if Z_l(2)==0
+        Z_l(2) = Inf;
     end
 else
     %BT1 Open
-    Z_l = [0 0 0]';
+    Z_l = 0;
     
     %Check CB4
     if CB(4) == 0
         %CB4 Open
-        Z_l = [par.motor1.Z_m par.motor1.Z_m par.motor1.Z_m]';
+        Z_l = par.motor1.Z_m;
     end
     
     %Check CB5
     if CB(5) == 0
         %CB5 Open
-        Z_l = Z_l + [par.motor2.Z_m par.motor2.Z_m par.motor2.Z_m]';
+        Z_l = Z_l + par.motor2.Z_m;
     end
     
     %Check CB6
     if CB(6) == 0
         %CB6 Open
-        Z_l = Z_l + [par.utility.Z_La_prime par.utility.Z_Lb_prime par.utility.Z_Lc_prime]';
+        Z_l = Z_l + par.utility.Z_Lc_prime;
     end
     
      %Check if there are no loads connected and if so impendance is infinite
-    if sum(Z_l==0) ~= 0
-        Z_l = [Inf Inf Inf]';
+    if Z_l==0
+        Z_l = Inf;
     end
 end
 end
--- thevenin_eq.m
+++ thevenin_eq.m
@@ -5,32 +5,35 @@ function [V_t Z_t] = thevenin_eq(par, CB, BT)
 %Check if Bus is connected
 if BT == 1
     %Bus is closed
+    %PORT
     if CB(1) == 1;
         %CB1 is closed
         V_t(:, 1) = [0 0 0]';
-        Z_t(:, 1) = [Inf Inf Inf]';
+        Z_t(1) = Inf;
     else
         %CB1 is open
         V_t(:, 1) = [par.genset1.E_a par.genset1.E_a par.genset1.E_a]';
-        Z_t(:, 1) = [par.genset1.Z_a par.genset1.Z_a par.genset1.Z_a]';
+        Z_t(1) = par.genset1.Z_a;
     end
+    
+    %STARBOARD
     
     if (CB(2) == 1) && (CB(3) == 1)
         %CB2 and CB3 is closed
         V_t(:, 2) = [0 0 0]';
-        Z_t(:, 2) = [Inf Inf Inf]';
+        Z_t(2) = Inf;
     end
     
     if (CB(2) == 0) && (CB(3) == 1)
         %CB2 is open and CB3 is closed
         V_t(:, 2) = [par.genset2.E_a par.genset2.E_a par.genset2.E_a]';
-        Z_t(:, 2) = [par.genset2.Z_a par.genset2.Z_a par.genset2.Z_a]';
+        Z_t(2) = par.genset2.Z_a;
     end
     
     if (CB(2) == 1) && (CB(3) == 0)
         %CB2 is closed and CB3 is open
         V_t(:, 2) = [par.genset3.E_a par.genset3.E_a par.genset3.E_a]';
-        Z_t(:, 2) = [par.genset3.Z_a par.genset3.Z_a par.genset3.Z_a]';
+        Z_t(2) = par.genset3.Z_a;
     end
     
     if (CB(2) == 0) && (CB(3) == 0)
@@ -38,33 +41,37 @@ if BT == 1
         temp_Z = (1/par.genset2.Z_a + 1/par.genset3.Z_a)^-1;
         temp_V = (par.genset2.E_a/par.genset2.Z_a + par.genset3.E_a/par.genset3.Z_a)*temp_Z;
         V_t(:, 2) = [temp_V temp_V temp_V]';
-        Z_t(:, 2) = [temp_Z temp_Z temp_Z]';
+        Z_t(2) = temp_Z;
     end
+    
+    %Phase shifting
+    V_t = V_t.*[1 1; -2*pi*1i/3 -2*pi*1i/3; 2*pi*1i/3 2*pi*1i/3];
+    
 else
     %Bus is open
     
     if (CB(1) == 1) && (CB(2) == 1) && (CB(3) == 1)
         %All Closed
         V_t = [0 0 0]';
-        Z_t = [Inf Inf Inf]';
+        Z_t = Inf;
     end
     
     if (CB(1) == 0) && (CB(2) == 1) && (CB(3) == 1)
         %CB1 open, CB2 and CB3 closed
         V_t = [par.genset1.E_a par.genset1.E_a par.genset1.E_a]';
-        Z_t = [par.genset1.Z_a par.genset1.Z_a par.genset1.Z_a]';
+        Z_t = par.genset1.Z_a;
     end
     
     if (CB(1) == 1) && (CB(2) == 0) && (CB(3) == 1)
         %CB1 closed, CB2 open, CB3 closed
         V_t = [par.genset2.E_a par.genset2.E_a par.genset2.E_a]';
-        Z_t = [par.genset2.Z_a par.genset2.Z_a par.genset2.Z_a]';
+        Z_t = par.genset2.Z_a;
     end
     
     if (CB(1) == 1) && (CB(2) == 1) && (CB(3) == 0)
         %CB1 closed, CB2 closed, CB3 open
         V_t = [par.genset3.E_a par.genset3.E_a par.genset3.E_a]';
-        Z_t = [par.genset3.Z_a par.genset3.Z_a par.genset3.Z_a]';
+        Z_t = par.genset3.Z_a;
     end
     
     if (CB(1) == 0) && (CB(2) == 0) && (CB(3) == 1)
@@ -72,7 +79,7 @@ else
         temp_Z = (1/par.genset1.Z_a + 1/par.genset2.Z_a)^-1;
         temp_V = (par.genset1.E_a/par.genset1.Z_a + par.genset2.E_a/par.genset2.Z_a)*temp_Z;
         V_t = [temp_V temp_V temp_V]';
-        Z_t = [temp_Z temp_Z temp_Z]';
+        Z_t = temp_Z;
     end
     
     if (CB(1) == 0) && (CB(2) == 1) && (CB(3) == 0)
@@ -80,7 +87,7 @@ else
         temp_Z = (1/par.genset1.Z_a + 1/par.genset3.Z_a)^-1;
         temp_V = (par.genset1.E_a/par.genset1.Z_a + par.genset3.E_a/par.genset3.Z_a)*temp_Z;
         V_t = [temp_V temp_V temp_V]';
-        Z_t = [temp_Z temp_Z temp_Z]';
+        Z_t = temp_Z;
     end
     
     if (CB(1) == 1) && (CB(2) == 0) && (CB(3) == 0)
@@ -88,7 +95,7 @@ else
         temp_Z = (1/par.genset2.Z_a + 1/par.genset3.Z_a)^-1;
         temp_V = (par.genset2.E_a/par.genset2.Z_a + par.genset3.E_a/par.genset3.Z_a)*temp_Z;
         V_t = [temp_V temp_V temp_V]';
-        Z_t = [temp_Z temp_Z temp_Z]';
+        Z_t = temp_Z;
     end
     
     if (CB(1) == 0) && (CB(2) == 0) && (CB(3) == 0)
@@ -96,7 +103,10 @@ else
         temp_Z = (1/par.genset1.Z_a + 1/par.genset2.Z_a + 1/par.genset3.Z_a)^-1;
         temp_V = (par.genset1.E_a/par.genset1.Z_a + par.genset2.E_a/par.genset2.Z_a + par.genset3.E_a/par.genset3.Z_a)*temp_Z;
         V_t = [temp_V temp_V temp_V]';
-        Z_t = [temp_Z temp_Z temp_Z]';
+        Z_t = temp_Z;
     end
+    
+    %Phase shifting
+    V_t = V_t.*[1; -2*pi*1i/3; 2*pi*1i/3];
 end
 
