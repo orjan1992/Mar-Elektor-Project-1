aa5e5d409cb0d12bd209cb411147b3105689fca5
orjan1992 <orjan1992@hotmail.com>
2017-02-12 14:48:28 +0100

    progress in load_params

--- run_prog.m
+++ run_prog.m
@@ -1,10 +1,10 @@
 clear
 init;
-CB = [0 0 0 0 0 0];
+CB = [0 0 0 0 1 0];
 BT = 1;
 Z_l = load_impedance(par, CB, BT);
 [V_t Z_t] = thevenin_eq(par, CB, BT);
 V_n = NVA(par, CB, BT, Z_l, V_t, Z_t);
-[ I_M1 I_M2 I_T1 I_n ] = load_params(par, CB, BT, V_n, Z_l);
+load_parameters = load_params(par, CB, BT, V_n, Z_l);
 print_phasor('V_n', V_n, 'V rms');
 print_phasor('V_t', V_t, 'V rms');
